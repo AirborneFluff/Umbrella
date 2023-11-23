83ea337a1f5cdd3808814376dbc3685ff795e56e
AirborneUnitcat <flanagan.a.97@gmail.com>
2023-11-23 22:59:17 +0000

    Added entities

--- API/Entities/StockItem.cs
+++ API/Entities/StockItem.cs
@@ -3,4 +3,5 @@
 public sealed class StockItem : SalesTransactionItem
 {
     public string? Location { get; set; }
+    public List<StockSupplySource> SupplySources { get; set; } = new List<StockSupplySource>();
 }
