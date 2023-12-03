0d0668dd90e7f0630816d4af1185b783c7a0861d
AirborneFluff <proffitt.m.98@gmail.com>
2023-12-03 17:07:18 +0000

    Added additional fields

--- API/Entities/StockSupplySource.cs
+++ API/Entities/StockSupplySource.cs
@@ -4,5 +4,10 @@ public sealed class StockSupplySource
 {
     public required string SupplierName { get; set; }
     public required string SupplierSKU { get; set; }
+    
+    public required string StockUnits { get; set; }
+    public decimal PackSize { get; set; }
+    public decimal MinimumOrderQuantity { get; set; }
+    
     public ICollection<PriceBreak> Prices { get; set; } = new List<PriceBreak>();
 }
