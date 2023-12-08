514b01c191789aa28da2c4d367ce9faae3269d6f
AirborneFluff <proffitt.m.98@gmail.com>
2023-12-08 20:08:14 +0000

    Added supply source price controller

---
+++ API/Controllers/StockItemsController.SupplySources.Prices.cs
@@ -0,0 +1,30 @@
+﻿using API.ActionFilters;
+using API.ActionResults;
+using API.Data.DTOs;
+using API.Entities;
+using API.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Cosmos;
+
+namespace API.Controllers;
+
+public sealed partial class StockItemsController
+{
+    [ServiceFilter(typeof(ValidateStockItemExists))]
+    [HttpPost("{partCode}/supplySources/{sourceIndex:int}/prices")]
+    public async Task<ActionResult> AddPriceBreak(NewPriceBreakDto price, string partCode, int sourceIndex)
+    {
+        var stockItem = HttpContext.GetStockItem();
+        var supplySource = stockItem.SupplySources.ElementAtOrDefault(sourceIndex);
+        if (supplySource is null) return BadRequest("Index out of range");
+
+        var newPriceBreak = _mapper.Map<PriceBreak>(price);
+        supplySource.Prices.Add(newPriceBreak);
+
+        var saveResult = await _unitOfWork.SaveChangesAsync();
+        if (saveResult.Success) return Ok(newPriceBreak);
+
+        if (saveResult.Exception is null) return BadRequest(saveResult.FailureMessage);
+        return new CosmosExceptionResult((CosmosException)saveResult.Exception);
+    }
+}
