a8b4e3ecc8282f0579a2ace7dcd9fcc51ee055e0
AirborneFluff <proffitt.m.98@gmail.com>
2023-12-08 20:18:27 +0000

    Added price break controller endpoints

--- API/Controllers/StockItemsController.SupplySources.Prices.cs
+++ API/Controllers/StockItemsController.SupplySources.Prices.cs
@@ -15,14 +15,57 @@ public sealed partial class StockItemsController
     public async Task<ActionResult> AddPriceBreak(NewPriceBreakDto price, string partCode, int sourceIndex)
     {
         var stockItem = HttpContext.GetStockItem();
+        
         var supplySource = stockItem.SupplySources.ElementAtOrDefault(sourceIndex);
-        if (supplySource is null) return BadRequest("Index out of range");
+        if (supplySource is null) return BadRequest("Supply Source index out of range");
 
         var newPriceBreak = _mapper.Map<PriceBreak>(price);
         supplySource.Prices.Add(newPriceBreak);
 
         var saveResult = await _unitOfWork.SaveChangesAsync();
         if (saveResult.Success) return Ok(newPriceBreak);
+
+        if (saveResult.Exception is null) return BadRequest(saveResult.FailureMessage);
+        return new CosmosExceptionResult((CosmosException)saveResult.Exception);
+    }
+    
+    [ServiceFilter(typeof(ValidateStockItemExists))]
+    [HttpPut("{partCode}/supplySources/{sourceIndex:int}/prices/{priceIndex:int}")]
+    public async Task<ActionResult> UpdatePriceBreak(NewPriceBreakDto price, string partCode, int sourceIndex, int priceIndex)
+    {
+        var stockItem = HttpContext.GetStockItem();
+        
+        var supplySource = stockItem.SupplySources.ElementAtOrDefault(sourceIndex);
+        if (supplySource is null) return BadRequest("Supply Source index out of range");
+
+        var priceBreak = supplySource.Prices.ElementAtOrDefault(priceIndex);
+        if (priceBreak is null) return BadRequest("Price index out of range");
+
+        _mapper.Map(price, priceBreak);
+
+        var saveResult = await _unitOfWork.SaveChangesAsync();
+        if (saveResult.Success) return Ok(priceBreak);
+
+        if (saveResult.Exception is null) return BadRequest(saveResult.FailureMessage);
+        return new CosmosExceptionResult((CosmosException)saveResult.Exception);
+    }
+    
+    [ServiceFilter(typeof(ValidateStockItemExists))]
+    [HttpDelete("{partCode}/supplySources/{sourceIndex:int}/prices/{priceIndex:int}")]
+    public async Task<ActionResult> RemovePriceBreak(string partCode, int sourceIndex, int priceIndex)
+    {
+        var stockItem = HttpContext.GetStockItem();
+        
+        var supplySource = stockItem.SupplySources.ElementAtOrDefault(sourceIndex);
+        if (supplySource is null) return BadRequest("Supply Source index out of range");
+
+        var priceBreak = supplySource.Prices.ElementAtOrDefault(priceIndex);
+        if (priceBreak is null) return BadRequest("Price index out of range");
+
+        supplySource.Prices.Remove(priceBreak);
+
+        var saveResult = await _unitOfWork.SaveChangesAsync();
+        if (saveResult.Success) return Ok();
 
         if (saveResult.Exception is null) return BadRequest(saveResult.FailureMessage);
         return new CosmosExceptionResult((CosmosException)saveResult.Exception);
