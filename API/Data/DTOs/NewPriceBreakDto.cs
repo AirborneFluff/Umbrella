91bcae52b96fef8b892a1cf6172503a08fc3ca8e
AirborneFluff <proffitt.m.98@gmail.com>
2023-12-08 20:10:16 +0000

    Fixed validation issue

--- API/Data/DTOs/NewPriceBreakDto.cs
+++ API/Data/DTOs/NewPriceBreakDto.cs
@@ -5,6 +5,6 @@ namespace API.Data.DTOs;
 public sealed class NewPriceBreakDto
 {
     [Required]
-    public decimal UnitCost { get; set; }
+    public decimal? UnitCost { get; set; }
     public int MinimumQuantity { get; set; } = 1;
 }
