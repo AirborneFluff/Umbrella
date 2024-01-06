5831ce94c0c163453a655b6853ad413f5ae8f25f
AirborneFluff <proffitt.m.98@gmail.com>
2024-01-06 21:47:15 +0000

    Infinite scrolling

--- client/src/app/features/stock/stock-list/stock-list.component.ts
+++ client/src/app/features/stock/stock-list/stock-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { StockService } from '../services/stock.service';
-import { map, Subject, Subscription, switchMap, take } from 'rxjs';
-import { PaginationParams } from '../../../core/utilities/pagination';
+import { BehaviorSubject, map, shareReplay, Subscription, switchMap, take } from 'rxjs';
+import { Pagination, PaginationParams } from '../../../core/utilities/pagination';
 import { StockItem } from '../../../core/models/stock-item';
 
 @Component({
@@ -20,30 +20,28 @@ export class StockListComponent implements OnInit, OnDestroy {
     this.subscriptions.add(this.dataStream$.subscribe(data => {
       this.stockItems.push(...data);
     }))
-
-    this.searchParams$.next({
-      pageNumber: 1,
-      pageSize: 50
-    })
   }
 
   ngOnDestroy() {
     this.subscriptions.unsubscribe();
   }
 
-  private searchParams$ = new Subject<PaginationParams>();
+  private searchParams$ = new BehaviorSubject<PaginationParams>({
+    pageNumber: 1,
+    pageSize: 50
+  });
 
-  private pageStream$ = this.searchParams$.pipe(switchMap(params => this.stockApi.getPaginatedList(params)));
-  private dataStream$ = this.pageStream$.pipe(map(result => result.result));
-  pagination$ = this.pageStream$.pipe(map(result => result.pagination));
+  private pageStream$ = this.searchParams$.pipe(switchMap(params => this.stockApi.getPaginatedList(params)), shareReplay(1));
+  private dataStream$ = this.pageStream$.pipe(map(result => result.result), shareReplay(1));
+  pagination$ = this.pageStream$.pipe(map(result => result.pagination), shareReplay(1));
 
   loadNextPage() {
     let params: PaginationParams;
+    let pagination: Pagination;
+    this.pagination$.pipe(take(1)).subscribe(page => pagination = page);
     this.searchParams$.pipe(take(1)).subscribe(search => params = search);
 
-    this.searchParams$.next({
-      pageSize: params!.pageSize,
-      pageNumber: params!.pageNumber
-    })
+    params!.pageNumber = pagination!.currentPage + 1;
+    this.searchParams$.next(params!);
   }
 }
