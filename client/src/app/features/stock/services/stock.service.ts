8c86ed5830d28639e47d66583199049ff182e808
AirborneFluff <proffitt.m.98@gmail.com>
2024-01-06 19:20:19 +0000

    Added basic api call

--- client/src/app/features/stock/services/stock.service.ts
+++ client/src/app/features/stock/services/stock.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../../environments/environment';
+import { Observable } from 'rxjs';
+import { StockItem } from '../../../core/models/stock-item';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StockService {
+  baseUrl = environment.apiUrl + 'stockItems/';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public getByPartCode(partCode: string): Observable<StockItem> {
+    return this.http.get<StockItem>(this.baseUrl + partCode);
+  }
 }
