c713cf7ff9edd67c78dda7a2cef398cda4082cd4
AirborneFluff <proffitt.m.98@gmail.com>
2024-01-07 20:36:42 +0000

    Added filter bottomsheet

--- client/src/app/features/stock/stock-list/stock-list.component.ts
+++ client/src/app/features/stock/stock-list/stock-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { StockService } from '../services/stock.service';
 import { BehaviorSubject, map, shareReplay, Subscription, switchMap, take, tap } from 'rxjs';
 import { Pagination } from '../../../core/utilities/pagination';
@@ -6,6 +6,10 @@ import { StockItem } from '../../../core/models/stock-item';
 import { BreakpointStream } from '../../../core/streams/breakpoint-stream';
 import { Breakpoints } from '../../../core/definitions/breakpoints';
 import { PagedSearchParams } from '../../../core/params/paged-search-params';
+import { MatBottomSheet } from '@angular/material/bottom-sheet';
+import { QueryFilterComponent } from '../../../shared/query-filter/query-filter/query-filter.component';
+import { OrbSearchComponent } from '../../../shared/orb-search/orb-search.component';
+import { FilterService } from '../../../shared/query-filter/services/filter.service';
 
 @Component({
   selector: 'app-stock-list',
@@ -13,11 +17,12 @@ import { PagedSearchParams } from '../../../core/params/paged-search-params';
   styleUrls: ['./stock-list.component.scss']
 })
 export class StockListComponent implements OnInit, OnDestroy {
+  @ViewChild('searchBar') searchBar!: OrbSearchComponent;
   stockItems: StockItem[] = [];
   subscriptions = new Subscription();
   pageSize = 50;
 
-  constructor(private stockApi: StockService, private breakpoint$: BreakpointStream) {
+  constructor(private stockApi: StockService, private breakpoint$: BreakpointStream, private bottomSheet: MatBottomSheet, private filter: FilterService) {
   }
 
   ngOnInit(): void {
@@ -71,5 +76,13 @@ export class StockListComponent implements OnInit, OnDestroy {
 
   private clearList() {
     this.stockItems = [];
+  }
+
+  openFilters() {
+    this.bottomSheet.open(QueryFilterComponent, {
+      panelClass: "h-4/5",
+      data: 'stockItem'
+    });
+    this.searchBar.collapse();
   }
 }
