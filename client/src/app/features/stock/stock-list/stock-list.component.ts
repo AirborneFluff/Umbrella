f2f36bfe95ff83f26d0b8dc7c4c422c9e6ba58c2
AirborneFluff <proffitt.m.98@gmail.com>
2024-01-07 16:29:20 +0000

    Efficiency change

--- client/src/app/features/stock/stock-list/stock-list.component.ts
+++ client/src/app/features/stock/stock-list/stock-list.component.ts
@@ -43,12 +43,15 @@ export class StockListComponent implements OnInit, OnDestroy {
 
   private pageStream$ = this.searchParams$.pipe(switchMap(params => this.stockApi.getPaginatedList(params)), shareReplay(1));
   private dataStream$ = this.pageStream$.pipe(map(result => result.result), shareReplay(1));
-  pagination$ = this.pageStream$.pipe(map(result => result.pagination), shareReplay(1));
+  private pagination$ = this.pageStream$.pipe(map(result => result.pagination), shareReplay(1));
 
   loadNextPage() {
-    let params: PagedSearchParams;
     let pagination: Pagination;
     this.pagination$.pipe(take(1)).subscribe(page => pagination = page);
+
+    if (pagination!.totalPages == pagination!.currentPage) return;
+
+    let params: PagedSearchParams;
     this.searchParams$.pipe(take(1)).subscribe(search => params = search);
 
     params!.pageNumber = pagination!.currentPage + 1;
