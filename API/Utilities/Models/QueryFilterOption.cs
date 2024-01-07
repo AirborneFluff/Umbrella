0f5d05e97cbefc8d9a9a5898636c7f164cbbc7c1
AirborneFluff <proffitt.m.98@gmail.com>
2024-01-07 19:46:52 +0000

    Added query filter to API

---
+++ API/Utilities/Models/QueryFilterOption.cs
@@ -0,0 +1,8 @@
+﻿namespace API.Utilities.Models;
+
+public sealed class QueryFilterOption
+{
+    public required string Title { get; set; }
+    public List<QueryFilterOption> Children { get; set; } = new List<QueryFilterOption>();
+    public QueryFilterParam? Parameter { get; set; }
+}
---
+++ API/Utilities/QueryFilterConfigBuilder.cs
@@ -0,0 +1,21 @@
+﻿using API.Utilities.Models;
+
+namespace API.Utilities;
+
+public static class QueryFilterConfigBuilder
+{
+    public static IEnumerable<QueryFilterOption> FromList (List<string> categories)
+    {
+        for (int i = 0; i < categories.Count(); i++)
+        {
+            yield return new QueryFilterOption()
+            {
+                Title = categories[i],
+                Parameter = new QueryFilterParam()
+                {
+                    Param = categories[i]
+                }
+            };
+        }
+    }
+}
