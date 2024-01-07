0758b53e3149be7486aad3605cf31d43f8ae5fd5
AirborneFluff <proffitt.m.98@gmail.com>
2024-01-07 15:11:19 +0000

    Added search output

--- client/src/app/shared/orb-search/orb-search.component.ts
+++ client/src/app/shared/orb-search/orb-search.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'orb-search',
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./orb-search.component.scss']
 })
 export class OrbSearchComponent {
+  @ViewChild('input') inputElement!: ElementRef;
+  @Output() searchTerm = new EventEmitter<string>();
   protected _expanded = false;
 
   public expand() {
@@ -14,5 +16,19 @@ export class OrbSearchComponent {
 
   public collapse() {
     this._expanded = false;
+  }
+
+  buttonClick() {
+    if (!this._expanded) {
+      this.expand();
+      return;
+    }
+
+    this.emitValue(this.inputElement.nativeElement.value);
+  }
+
+  emitValue(value: string) {
+    this.searchTerm.emit(value);
+    this.collapse();
   }
 }
