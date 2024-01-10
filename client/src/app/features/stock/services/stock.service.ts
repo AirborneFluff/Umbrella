911e1b50936b8f2b47040aa0f952352e55b01d8f
AirborneFluff <proffitt.m.98@gmail.com>
2024-01-10 22:58:22 +0000

    Added params helper

--- client/src/app/features/stock/services/stock.service.ts
+++ client/src/app/features/stock/services/stock.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { environment } from '../../../../environments/environment';
 import { Observable } from 'rxjs';
 import { StockItem } from '../../../core/models/stock-item';
 import { PaginatedResult } from '../../../core/utilities/pagination';
 import { getPaginatedResult, getPaginationHeaders } from '../../../core/utilities/pagination-helper';
 import { PagedSearchParams } from '../../../core/params/paged-search-params';
+import { mergeParams } from '../../../core/utilities/http-params-helper';
 
 @Injectable({
   providedIn: 'root'
@@ -19,8 +20,13 @@ export class StockService {
     return this.http.get<StockItem>(this.baseUrl + partCode);
   }
 
-  public getPaginatedList(stockParams: PagedSearchParams): Observable<PaginatedResult<StockItem[]>> {
+  public getPaginatedList(stockParams: PagedSearchParams, additionalParams?: HttpParams): Observable<PaginatedResult<StockItem[]>> {
     let params = getPaginationHeaders(stockParams.pageNumber, stockParams.pageSize);
+
+    if (additionalParams) {
+      params = mergeParams(params, additionalParams);
+    }
+
     if (stockParams.searchTerm) params = params.set('searchTerm', stockParams.searchTerm);
     return getPaginatedResult<StockItem[]>(this.baseUrl, params, this.http);
   }
