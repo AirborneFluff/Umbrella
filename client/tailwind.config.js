dbd867f994a564eeaff8226d4ee171ac8e292346
AirborneFluff <proffitt.m.98@gmail.com>
2024-01-10 21:10:32 +0000

    Color updates

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -8,6 +8,13 @@ module.exports = {
       sans: ['Open Sans', 'Helvetica', 'Arial', 'sans-serif'],
       display: ['Poppins', 'Helvetica', 'Arial', 'sans-serif'],
     },
+    colors: {
+      'primary': '#5B7CFD',
+      'base-light': '#E7EEF7',
+      'item-light': '#FFFFFF',
+      'item-faded-light': '#F5F6F8',
+      // Configure your color palette here
+    },
     extend: {},
   },
   plugins: [],
