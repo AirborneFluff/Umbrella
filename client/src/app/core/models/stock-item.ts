fed917e6c3ff2be9f5032c3d8162f3e1ebe833d6
AirborneFluff <proffitt.m.98@gmail.com>
2024-01-14 14:41:10 +0000

    Updated model

--- client/src/app/core/models/stock-item.ts
+++ client/src/app/core/models/stock-item.ts
@@ -3,6 +3,7 @@ import { StockSupplySource } from './stock-supply-source';
 export interface StockItem {
   partCode: string,
   description: string,
-  location: string,
+  location: string | undefined,
+  category: string | undefined,
   supplySources: StockSupplySource[]
 }
