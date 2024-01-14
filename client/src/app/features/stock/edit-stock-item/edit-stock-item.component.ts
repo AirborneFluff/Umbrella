cc0b8e90bc2d8b69c3967516bd0d58a98bfe437c
AirborneFluff <proffitt.m.98@gmail.com>
2024-01-14 16:22:52 +0000

    Added ghost form

--- client/src/app/features/stock/edit-stock-item/edit-stock-item.component.ts
+++ client/src/app/features/stock/edit-stock-item/edit-stock-item.component.ts
@@ -1,27 +1,25 @@
-import { Component, ViewChild } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, ViewChild } from '@angular/core';
 import { StockItemFormComponent } from '../stock-item-form/stock-item-form.component';
 import { StockService } from '../services/stock.service';
 import { StockItemIdStream } from '../../../core/streams/stock-item-id-stream';
 import { notNullOrUndefined } from '../../../core/pipes/not-null';
-import { Subscription, switchMap } from 'rxjs';
+import { shareReplay, switchMap, take } from 'rxjs';
 
 @Component({
   selector: 'app-edit-stock-item',
   templateUrl: './edit-stock-item.component.html',
   styleUrls: ['./edit-stock-item.component.scss']
 })
-export class EditStockItemComponent {
-  @ViewChild(StockItemFormComponent) formComponent!: StockItemFormComponent;
-  private subscriptions = new Subscription();
+export class EditStockItemComponent implements AfterViewInit {
+  @ViewChild(StockItemFormComponent, {static: false}) formComponent!: StockItemFormComponent;
 
-  constructor(private stockApi: StockService, private id$: StockItemIdStream) {
-    this.subscriptions.add(
-      this.stockItem$.subscribe(item => this.formComponent.patchForm(item)));
+  constructor(private stockApi: StockService, private id$: StockItemIdStream, private changeDetector: ChangeDetectorRef) {
   }
 
   stockItem$ = this.id$.pipe(
     notNullOrUndefined(),
-    switchMap(id => this.stockApi.getById(id))
+    switchMap(id => this.stockApi.getById(id)),
+    shareReplay(1)
   )
 
   save() {
@@ -29,6 +27,15 @@ export class EditStockItemComponent {
     // this.stockApi.addNew(item).subscribe({
     //   next: newItem => console.log(newItem)
     // })
+  }
+
+  ngAfterViewInit(): void {
+    this.stockItem$
+      .pipe(take(1))
+      .subscribe(item => {
+        this.changeDetector.detectChanges();
+        this.formComponent.patchForm(item)
+      });
   }
 
 }
