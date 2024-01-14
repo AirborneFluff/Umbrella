9f3f54abcefa94b600a805e71318465d3cb16595
AirborneFluff <proffitt.m.98@gmail.com>
2024-01-14 12:55:13 +0000

    Added input directive

--- client/src/app/shared/app-forms/app-forms.module.ts
+++ client/src/app/shared/app-forms/app-forms.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormFieldComponent } from './form-field/form-field.component';
+import { AppInputDirective } from './directives/app-input.directive';
 
 
 
 @NgModule({
   declarations: [
-    FormFieldComponent
+    FormFieldComponent,
+    AppInputDirective
+  ],
+  exports: [
+    FormFieldComponent,
+    AppInputDirective
   ],
   imports: [
     CommonModule
