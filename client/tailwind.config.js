a15bc3ff7d5c2ad956000169c73988a5bb0ca62d
AirborneFluff <proffitt.m.98@gmail.com>
2024-01-15 19:40:52 +0000

    Added colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -11,6 +11,8 @@ module.exports = {
     extend: {
       colors: {
         'primary': '#5B7CFD',
+        'primary-highlight': '#5270e4',
+        'primary-deep': '#2e3e7f',
         'base-light': '#E7EEF7',
         'item-light': '#FFFFFF',
         'item-faded-light': '#F5F6F8'
