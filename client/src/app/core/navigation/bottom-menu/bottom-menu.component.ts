64a14698d7a585b89d13afad30e7e0392f9058a8
AirborneFluff <proffitt.m.98@gmail.com>
2024-01-15 19:43:32 +0000

    Added root observable

--- client/src/app/core/navigation/bottom-menu/bottom-menu.component.ts
+++ client/src/app/core/navigation/bottom-menu/bottom-menu.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { RootFeatureStream } from '../../streams/root-feature-stream';
+import { notNullOrUndefined } from '../../pipes/not-null';
+import { RootFeatures } from '../../definitions/root-features';
 
 @Component({
   selector: 'app-bottom-menu',
@@ -6,4 +9,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./bottom-menu.component.scss']
 })
 export class BottomMenuComponent {
+
+  constructor(private root$: RootFeatureStream) {}
+
+  protected activeRoot$ = this.root$.pipe(notNullOrUndefined())
+
+  protected readonly RootFeatures = RootFeatures;
 }
