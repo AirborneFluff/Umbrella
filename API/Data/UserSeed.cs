bbae1beb706f4c6b0f8f59737a899ce545e98de4
AirborneFluff <proffitt.m.98@gmail.com>
2024-01-16 22:58:36 +0000

    Added demo account to seed

--- API/Data/UserSeed.cs
+++ API/Data/UserSeed.cs
@@ -19,7 +19,6 @@ public sealed class UserSeed
 
         if (ownerUserName is null || ownerPassword is null|| organisationName is null)
             throw new Exception("Owner UserName and Password not configured");
-
         
         var ownerId = Guid.NewGuid().ToString();
         
@@ -38,8 +37,18 @@ public sealed class UserSeed
             Organisation = organisation,
             OrganisationId = organisation.Id
         };
+
+        var demoUser = new AppUser()
+        {
+            UserName = "demo",
+            Email = "demo",
+            Permissions = PermissionGroups.ReadOnlyUser,
+            OrganisationId = organisation.Id
+        };
         
         organisation.Members.Add(owner);
+        organisation.Members.Add(demoUser);
+        await userManager.CreateAsync(demoUser, "Demologin@1");
         
         var result = await userManager.CreateAsync(owner, ownerPassword);
         if (!result.Succeeded) throw new Exception("Error adding seed account");
