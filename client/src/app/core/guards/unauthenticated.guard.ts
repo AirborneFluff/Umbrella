8b17ea93ca5cccc34a31373352fe86703b066568
AirborneFluff <proffitt.m.98@gmail.com>
2024-01-16 20:03:30 +0000

    Added async guards

--- client/src/app/core/guards/unauthenticated.guard.ts
+++ client/src/app/core/guards/unauthenticated.guard.ts
@@ -1,15 +1,15 @@
 import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
 import { AccountService } from '../services/account.service';
-import { map, take, tap } from 'rxjs';
+import { map, take } from 'rxjs';
 
-export const loggedInGuard: CanActivateFn = (route, state) => {
+export const unauthenticatedGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
   return inject(AccountService).currentUser$.pipe(
     take(1),
     map(user => {
-      const canActivate = !!user;
+      const canActivate = !user;
       if (canActivate) return true;
-      return router.parseUrl('');
+      return router.parseUrl('/app');
     }));
 };
