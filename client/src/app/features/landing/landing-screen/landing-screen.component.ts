30ea8bf8b3d3d9499a91eba17e80be90c6991461
AirborneFluff <proffitt.m.98@gmail.com>
2024-01-16 23:06:43 +0000

    Added demo login

--- client/src/app/features/landing/landing-screen/landing-screen.component.ts
+++ client/src/app/features/landing/landing-screen/landing-screen.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AccountService } from '../../../core/services/account.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing-screen',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./landing-screen.component.scss']
 })
 export class LandingScreenComponent {
+
+  constructor(private account: AccountService, private router: Router) {
+  }
+
+  demoLogin() {
+    this.account.login({
+      email: 'demo',
+      password: 'Demologin@1'
+    }).subscribe({
+      next: () => this.router.navigateByUrl('/app'),
+      error: e => console.log(e)
+    })
+  }
 
 }
