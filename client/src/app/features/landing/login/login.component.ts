c260b442acfcef4517e4ea9ae3fc756f750c76fc
AirborneFluff <proffitt.m.98@gmail.com>
2024-01-16 20:46:00 +0000

    Added login functionality

--- client/src/app/features/landing/login/login.component.ts
+++ client/src/app/features/landing/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { LoginParams } from '../../../core/models/login-params';
+import { AccountService } from '../../../core/services/account.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,14 +10,24 @@ import { LoginParams } from '../../../core/models/login-params';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  busy = false;
   protected form = new FormGroup({
     email: new FormControl<string>('', {validators: [Validators.required], nonNullable: true}),
     password: new FormControl<string>('', {validators: [Validators.required], nonNullable: true}),
   })
 
-  constructor() {}
+  constructor(private account: AccountService, private router: Router) {}
 
   get formValue(): LoginParams {
     return this.form.getRawValue() as LoginParams;
   }
+
+  login() {
+    this.busy = true;
+    this.account.login(this.formValue).subscribe({
+      next: () => this.router.navigateByUrl('/app'),
+      error: e => console.log(e)
+    })
+  }
+
 }
