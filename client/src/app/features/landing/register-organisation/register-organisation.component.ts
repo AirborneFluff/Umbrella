8e66e5d7893b32c5c0b7ab5c4afa77f9e265ba3b
AirborneFluff <proffitt.m.98@gmail.com>
2024-01-17 21:43:25 +0000

    Added validation errors

--- client/src/app/features/landing/register-organisation/register-organisation.component.ts
+++ client/src/app/features/landing/register-organisation/register-organisation.component.ts
@@ -1,28 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { RegisterOrganisationParams } from '../../../core/models/register-organisation-params';
 import { OrganisationService } from '../../../core/services/organisation.service';
+import { matchValues } from '../../../core/form-validators/match-values-validator';
+import { Subscription } from 'rxjs';
+import { passwordStrengthValidator } from '../../../core/form-validators/password-strength-validator';
 
 @Component({
   selector: 'app-register-organisation',
   templateUrl: './register-organisation.component.html',
   styleUrls: ['./register-organisation.component.scss']
 })
-export class RegisterOrganisationComponent {
+export class RegisterOrganisationComponent implements OnDestroy {
   busy = false;
   success: boolean | undefined;
 
+  subscriptions = new Subscription();
+
   protected form = new FormGroup({
-    organisationName: new FormControl<string>('', {validators: [Validators.required], nonNullable: true}),
-    email: new FormControl<string>('', {validators: [Validators.required], nonNullable: true}),
-    password: new FormControl<string>('', {validators: [Validators.required], nonNullable: true}),
+    organisationName: new FormControl<string>('', {
+      validators: [Validators.required], nonNullable: true
+    }),
+    email: new FormControl<string>('', {
+      validators: [Validators.required], nonNullable: true
+    }),
+    password: new FormControl<string>('', {
+      validators: [Validators.required, Validators.minLength(6), passwordStrengthValidator()], nonNullable: true
+    }),
+    confirmPassword: new FormControl<string>('', {
+      validators: [Validators.required, matchValues('password')], nonNullable: true
+    }),
   })
 
-  constructor(private organisation: OrganisationService) {}
+  constructor(private organisation: OrganisationService) {
+    this.subscriptions.add(
+      this.form.controls['password'].valueChanges.subscribe(() => {
+        this.form.controls['confirmPassword'].updateValueAndValidity();
+      })
+    )
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.unsubscribe();
+  }
 
   get formValue(): RegisterOrganisationParams {
     return this.form.getRawValue() as RegisterOrganisationParams;
   }
+
 
   register() {
     this.busy = true;
