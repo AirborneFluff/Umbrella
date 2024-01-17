2d442085c0af851a6f9e1e029ed94e6d3121020c
AirborneFluff <proffitt.m.98@gmail.com>
2024-01-17 23:43:13 +0000

    Spinners stop on observable complete

--- client/src/app/features/stock/new-stock-item/new-stock-item.component.ts
+++ client/src/app/features/stock/new-stock-item/new-stock-item.component.ts
@@ -2,6 +2,7 @@ import { Component, ViewChild } from '@angular/core';
 import { StockItemFormComponent } from '../stock-item-form/stock-item-form.component';
 import { StockService } from '../../../core/services/stock.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { finalize } from 'rxjs';
 
 @Component({
   selector: 'app-new-stock-item',
@@ -20,8 +21,9 @@ export class NewStockItemComponent {
     const item = this.formComponent.formValue;
     this.busy = true;
 
-    this.stockApi.addNew(item).subscribe(() => {
-      this.busy = false;
+    this.stockApi.addNew(item).pipe(
+      finalize(() => this.busy = false)
+    ).subscribe(() => {
       this.router.navigate(['../'], { relativeTo: this.route })
     })
   }
