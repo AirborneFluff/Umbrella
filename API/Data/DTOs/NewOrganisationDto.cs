44d25557ded107f8f05b2995c5583531c8fbdb0d
AirborneFluff <proffitt.m.98@gmail.com>
2024-01-17 21:52:28 +0000

    Enforces password and name lengths

--- API/Data/DTOs/NewOrganisationDto.cs
+++ API/Data/DTOs/NewOrganisationDto.cs
@@ -5,10 +5,13 @@ namespace API.Data.DTOs;
 public sealed class NewOrganisationDto
 {
     [Required]
+    [StringLength(32, MinimumLength = 3)]
     public required string OrganisationName { get; set; }
     
     [Required]
     public required string Email { get; set; }
+    
     [Required]
+    [StringLength(32, MinimumLength = 6)]
     public required string Password { get; set; }
 }
