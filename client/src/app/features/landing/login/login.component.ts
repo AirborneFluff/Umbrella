35a35e28a09a447526c6c11e4f82b6e6a1094274
AirborneFluff <proffitt.m.98@gmail.com>
2024-01-17 20:38:45 +0000

    Added error message

--- client/src/app/features/landing/login/login.component.ts
+++ client/src/app/features/landing/login/login.component.ts
@@ -3,6 +3,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { LoginParams } from '../../../core/models/login-params';
 import { AccountService } from '../../../core/services/account.service';
 import { Router } from '@angular/router';
+import { catchError, delay, finalize } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -11,6 +12,8 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent {
   busy = false;
+  error: string | undefined;
+
   protected form = new FormGroup({
     email: new FormControl<string>('', {validators: [Validators.required], nonNullable: true}),
     password: new FormControl<string>('', {validators: [Validators.required], nonNullable: true}),
@@ -24,9 +27,11 @@ export class LoginComponent {
 
   login() {
     this.busy = true;
-    this.account.login(this.formValue).subscribe({
+    this.account.login(this.formValue).pipe(
+      finalize(() => this.busy = false)
+    ).subscribe({
       next: () => this.router.navigateByUrl('/app'),
-      error: e => console.log(e)
+      error: e => this.error = e.error
     })
   }
 }
