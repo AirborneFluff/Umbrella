cdc6d92d3b91d35b69a29f0c334a7e1993c8d58e
AirborneFluff <proffitt.m.98@gmail.com>
2024-01-19 00:16:42 +0000

    Added additional methods with tests

--- API/Extensions/DictionaryExtensions.cs
+++ API/Extensions/DictionaryExtensions.cs
@@ -6,4 +6,26 @@ public static class DictionaryExtensions
     {
         return dictionary.TryGetValue(key, out var value) ? value : throw new Exception(errorMessage);
     }
+
+    public static void AddWithQuantity(this Dictionary<string, int> dictionary, string key)
+    {
+        if (dictionary.ContainsKey(key))
+        {
+            dictionary[key] += 1;
+            return;
+        }
+        dictionary.Add(key, 1);
+    }
+
+    public static void RemoveWithQuantity(this Dictionary<string, int> dictionary, string key)
+    {
+        if (!dictionary.TryGetValue(key, out var currentValue)) return;
+        
+        if (currentValue == 1)
+        {
+            dictionary.Remove(key);
+            return;
+        }
+        dictionary[key] -= 1;
+    }
 }
---
+++ API_Tests/Extensions/DictionaryExtensionsTest.cs
@@ -0,0 +1,182 @@
+﻿using API.Extensions;
+using JetBrains.Annotations;
+
+namespace API_Tests.Extensions;
+
+[TestClass]
+[TestSubject(typeof(DictionaryExtensions))]
+public class DictionaryExtensionsTest
+{
+
+    [TestMethod]
+    public void AddWithQuantity_ShouldReturnWithNewItem()
+    {
+        // Arrange
+        var dictionary = new Dictionary<string, int>()
+        {
+            { "Dairy", 1 },
+            { "Electrical", 8 },
+            { "Vegetables", 3 },
+        };
+        
+        var expected = new Dictionary<string, int>()
+        {
+            { "Dairy", 1 },
+            { "Electrical", 8 },
+            { "Vegetables", 3 },
+            { "Clothing", 1 },
+        };
+
+        var additionalItem = "Clothing";
+        
+        // Act
+        dictionary.AddWithQuantity(additionalItem);
+        
+        // Assert
+        Assert.IsTrue(Equals(dictionary, expected));
+    }
+    
+    [TestMethod]
+    public void AddWithQuantity_ShouldReturnWithNewValue()
+    {
+        // Arrange
+        var dictionary = new Dictionary<string, int>()
+        {
+            { "Dairy", 1 },
+            { "Electrical", 8 },
+            { "Vegetables", 3 },
+        };
+        
+        var expected = new Dictionary<string, int>()
+        {
+            { "Dairy", 2 },
+            { "Electrical", 8 },
+            { "Vegetables", 3 },
+        };
+
+        var additionalItem = "Dairy";
+        
+        // Act
+        dictionary.AddWithQuantity(additionalItem);
+        
+        // Assert
+        Assert.IsTrue(Equals(dictionary, expected));
+    }
+    
+    [TestMethod]
+    public void RemoveWithQuantity_ShouldReturnWithNewValue()
+    {
+        // Arrange
+        var dictionary = new Dictionary<string, int>()
+        {
+            { "Dairy", 1 },
+            { "Electrical", 8 },
+            { "Vegetables", 3 },
+        };
+        
+        var expected = new Dictionary<string, int>()
+        {
+            { "Dairy", 1 },
+            { "Electrical", 7 },
+            { "Vegetables", 3 },
+        };
+
+        var itemToRemove = "Electrical";
+        
+        // Act
+        dictionary.RemoveWithQuantity(itemToRemove);
+        
+        // Assert
+        Assert.IsTrue(Equals(dictionary, expected));
+    }
+    
+    [TestMethod]
+    public void RemoveWithQuantity_ShouldReturnWithNewItemRemoved()
+    {
+        // Arrange
+        var dictionary = new Dictionary<string, int>()
+        {
+            { "Dairy", 1 },
+            { "Electrical", 8 },
+            { "Vegetables", 3 },
+        };
+        
+        var expected = new Dictionary<string, int>()
+        {
+            { "Electrical", 8 },
+            { "Vegetables", 3 },
+        };
+
+        var itemToRemove = "Dairy";
+        
+        // Act
+        dictionary.RemoveWithQuantity(itemToRemove);
+        
+        // Assert
+        Assert.IsTrue(Equals(dictionary, expected));
+    }
+    
+    [TestMethod]
+    public void RemoveWithQuantity_ShouldReturnUnchanged()
+    {
+        // Arrange
+        var dictionary = new Dictionary<string, int>()
+        {
+            { "Dairy", 1 },
+            { "Electrical", 8 },
+            { "Vegetables", 3 },
+        };
+        
+        var expected = new Dictionary<string, int>()
+        {
+            { "Dairy", 1 },
+            { "Electrical", 8 },
+            { "Vegetables", 3 },
+        };
+
+        var itemToRemove = "";
+        
+        // Act
+        dictionary.RemoveWithQuantity(itemToRemove);
+        
+        // Assert
+        Assert.IsTrue(Equals(dictionary, expected));
+    }
+    
+    [TestMethod]
+    public void RemoveWithQuantity_ShouldReturnUnchanged_1()
+    {
+        // Arrange
+        var dictionary = new Dictionary<string, int>()
+        {
+            { "Dairy", 1 },
+            { "Electrical", 8 },
+            { "Vegetables", 3 },
+        };
+        
+        var expected = new Dictionary<string, int>()
+        {
+            { "Dairy", 1 },
+            { "Electrical", 8 },
+            { "Vegetables", 3 },
+        };
+
+        var itemToRemove = "Random";
+        
+        // Act
+        dictionary.RemoveWithQuantity(itemToRemove);
+        
+        // Assert
+        Assert.IsTrue(Equals(dictionary, expected));
+    }
+    
+    private bool Equals(IDictionary<string, int> x, IDictionary<string, int> y)
+    {
+        if (x.Count != y.Count)
+            return false;
+
+        HashSet<KeyValuePair<string, int>> set = new HashSet<KeyValuePair<string, int>>(x);
+        set.SymmetricExceptWith(y);
+        return set.Count == 0;
+    }
+}
